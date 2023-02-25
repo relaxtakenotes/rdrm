561d4cfed68f093be18cc5ae6cb6f248c007f673
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2023-02-25 16:08:26 +0500

    remove pesky timewarp hook

--- red_dead_redemption_mechanics/lua/autorun/sh_rdrm.lua
+++ red_dead_redemption_mechanics/lua/autorun/sh_rdrm.lua
@@ -2,3 +2,7 @@ if SERVER then print("sh_rdrm.lua loaded on server") end
 if CLIENT then print("sh_rdrm.lua loaded on client") end
 
 rdrm = {}
+
+timer.Simple(15, function() 
+	hook.Remove("EntityEmitSound", "ARC9_TimeWarpSounds") // PLEASE NO MORE
+end)
