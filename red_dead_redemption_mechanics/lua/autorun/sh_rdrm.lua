31f2b46e69e37bd58d5b0c6d42f0a46131673257
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2023-09-10 11:12:34 +0500

    another one

--- red_dead_redemption_mechanics/lua/autorun/sh_rdrm.lua
+++ red_dead_redemption_mechanics/lua/autorun/sh_rdrm.lua
@@ -3,6 +3,9 @@ if CLIENT then print("sh_rdrm.lua loaded on client") end
 
 rdrm = {}
 
-timer.Simple(15, function() 
-	hook.Remove("EntityEmitSound", "ARC9_TimeWarpSounds") // PLEASE NO MORE
+timer.Simple(15, function()
+	hook.Remove("EntityEmitSound", "zzz_TFA_EntityEmitSound")
+	hook.Remove("EntityEmitSound", "ARC9_TimeWarpSounds") 
+	hook.Remove("EntityEmitSound", "JMOD_EntityEmitSound") // >timescale sound pitch scaling should be a part of gmod by default - jmod
+														   // only if it didn't sound like shit and could be bypassed in the emitsound function!
 end)
